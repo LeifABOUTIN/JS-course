5abb67d91b809f967b45a70115b783a7feaf0020
LeifABOUTIN <leif.boutin@epitech.eu>
2021-02-17 10:17:03 +0100

    finished tempo project

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ class Drumkit{
     constructor(){
         this.pads = document.querySelectorAll('.pad')
         this.playBtn = document.querySelector('.play')
+        this.currentKick = './sounds/kick-classic.mp4'
+        this.currentSnare = './sounds/snare-acoustic01.mp4'
+        this.currentHihat = './sounds/hihat-acoustic01.mp4'
         this.kickAudio = document.querySelector('.kick-sound')
         this.snareAudio = document.querySelector('.snare-sound')
         this.hihatAudio = document.querySelector('.hihat-sound')
         this.index = 0
         this.bpm = 150
+        this.isPlaying = null
+        this.selects = document.querySelectorAll('select')
+        this.muteBtns = document.querySelectorAll('.mute')
+        this.tempoSlider = document.querySelector('.tempo-slider')
     }
     activePad(){
         this.classList.toggle('active')
@@ -39,13 +46,93 @@ class Drumkit{
     }
     start(){
         const interval = (60/this.bpm) * 1000
-        setInterval(() => {
+        //check if it's playing
+        if(this.isPlaying){
+        clearInterval(this.isPlaying)
+        this.isPlaying = null
+        }
+        else{
+        //clear interval
+        this.isPlaying = setInterval(() => {
             this.repeat()
-        }, interval)
+        }, interval)}
+    }
+    
+    updataBtn(){
+        if(!this.isPlaying){
+            this.playBtn.innerText = "Stop"
+            this.playBtn.classList.add('active')
+        }
+        else{
+            this.playBtn.innerText = "Play"
+            this.playBtn.classList.remove('active')
+        }
+    }
+    changeSound(e){
+        const selectionName = e.target.name
+        const selectionValue = e.target.value
+        console.log(selectionName)
+        console.log(selectionValue)
+        switch(selectionName){
+            case "kick-select":
+                this.kickAudio.src = selectionValue
+                break
+            case "snare-select":
+                this.snareAudio.src = selectionValue
+                break
+            case "hihat-select":
+                this.hihatAudio.src = selectionValue
+                break
+        }
+
+    }
+    mute(e){
+        const muteIndex = e.target.getAttribute('data-track')
+        e.target.classList.toggle('active')
+        if(e.target.classList.contains('active')){
+            switch(muteIndex){
+                case "0":
+                    this.kickAudio.volume = 0;
+                    break
+                case "1":
+                    this.snareAudio.volume = 0;
+                    break
+                case "2":
+                    this.hihatAudio.volume = 0;
+                    break
+            }
+        }else{
+            switch(muteIndex){
+                case "0":
+                    this.kickAudio.volume = 1;
+                    break
+                case "1":
+                    this.snareAudio.volume = 1;
+                    break
+                case "2":
+                    this.hihatAudio.volume = 1;
+                    break
+            }
+        }
+    }
+    changeTempo(e){
+        const tempoText = document.querySelector('.tempo-nr')
+        tempoText.innerText = e.target.value 
+    }
+    updateTempo(e){
+        this.bpm = e.target.value 
+        clearInterval(this.isPlaying)
+        this.isPlaying = null
+        const playBtn = document.querySelector('.play')
+        if(playBtn.classList.contains('active')){
+            this.start()
+        }
     }
 }
 
 const drumKit = new Drumkit();
+
+//events listeners
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad)
@@ -54,5 +141,25 @@ drumKit.pads.forEach(pad => {
     })
 })
 drumKit.playBtn.addEventListener('click', () => {
+    drumKit.updataBtn()
     drumKit.start()
+})
+
+drumKit.selects.forEach(select => {
+    select.addEventListener('change', function(e){
+        drumKit.changeSound(e)
+    })
+})
+
+drumKit.muteBtns.forEach(btn => {
+    btn.addEventListener('click', function(e){
+        drumKit.mute(e)
+    })
+})
+
+drumKit.tempoSlider.addEventListener('input', function(e){
+    drumKit.changeTempo(e)
+})
+drumKit.tempoSlider.addEventListener('change', function(e){
+    drumKit.updateTempo(e)
 })
